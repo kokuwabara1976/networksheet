221400d1fc359d099faf26617198ea030b2ac028
Ko Kuwabara <kokuwabara@gmail.com>
2021-06-02 18:27:12 +0800

    PDF

--- static/js/custom.js
+++ static/js/custom.js
@@ -65,42 +65,42 @@ function generatePDF() {
 	doc.text(0.4, 3.9, str);
 	doc.text(0.4, 4.5, "Do you see any weak relationships? In what ways are they weak?");
 
-	doc.setFontSize(13);
+	doc.setFontSize(11);
     doc.setFont("roboto_bold");
     doc.setTextColor(46, 108, 83);
-    doc.text(0.4, 4.8, "Response keys");
+    doc.text(0.6, 4.8, "Response keys");
 
     doc.setFontSize(10);
     doc.setFont("roboto_light");
     doc.setTextColor(32, 33, 36);
-    doc.text(0.4, 5.0, "How close are you?");
+    doc.text(0.6, 5.0, "How close are you?");
 
     doc.setFontSize(10);
     doc.setFont("roboto_light");
     doc.setTextColor(32, 33, 36);
-    doc.text(0.4, 5.2, "How often do you talk during work?");
+    doc.text(0.6, 5.2, "How often do you talk during work?");
 
 	doc.setFontSize(10);
     doc.setFont("roboto_light");
     doc.setTextColor(32, 33, 36);
-    doc.text(0.4, 5.4, "How often do you talk outside of work?");
+    doc.text(0.6, 5.4, "How often do you talk outside of work?");
 
     doc.setFontSize(10);
     doc.setFont("roboto_light");
     doc.setTextColor(32, 33, 36);
-    doc.text(0.4, 5.6, "How balanced are your relationships?");
+    doc.text(0.6, 5.6, "How balanced are your relationships?");
 
 	doc.setFontSize(10);
     doc.setFont("roboto_light");
     doc.setTextColor(32, 33, 36);
-    doc.text(0.4, 5.8, "What do you know abou them?");
+    doc.text(0.6, 5.8, "What do you know about them?");
 
 	doc.setFontSize(10);
     doc.setFont("roboto_light");
     doc.setTextColor(32, 33, 36);
-    doc.text(0.4, 6.0, "How similar are you?");
+    doc.text(0.6, 6.0, "How similar are you?");
 
-doc.addPage("", "");
+	//doc.addPage("", "");
 
 	str = [
 		"Contacts",
@@ -116,7 +116,7 @@ doc.addPage("", "");
 	for (var i = 0; i < 7; i++) {
 		doc.setDrawColor(0);
 		doc.setFillColor(240, 240, 240);
-		var x = i; //0.4 + 1.09 * i;
+		var x = 0.4 + 1.09 * i;
 		doc.rect(x, 4.8, 1.06, 0.85, "F");
 		doc.text(x+0.05, 5, str[i]);
 	}
